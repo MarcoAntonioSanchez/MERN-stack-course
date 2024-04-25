d6f4639fd9df3685c2cc552d293b9640310de4a7
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2024-04-25 13:39:59 -0600

    3. Create routes in node practice done, with if conditionals for root and about routes responses

--- node-server.js
+++ node-server.js
@@ -1,7 +1,19 @@
 const http = require("http"); // No installation of package needed, just module import as it already comes along with Node.js
 
 const server = http.createServer((req, res) => { // Create a server and save it on variable with a req, res callback.
-    console.log("Node server up!"); // This will be printed when a client visits: http://localhost:5000
+    // console.log("Node server up!"); // This will be printed when a client visits: http://localhost:5000
+    // console.log(req.url); // This prints out the '/' url route, example: http://localhost:5000/.
+    // console.log(req.method); // This prints out the GET method when a browser hits the '/' url / route.
+    if (req.url === "/") { // When the root route "/" is visited, then...
+        res.setHeader("Content-Type", "text/html") // Set headers, for the next response function to work.
+        res.write("<h1>Home page...</h1>"); // Write as HTML, thanks to content type and html atributes.
+        res.end(); // End of the response.
+    }
+    if (req.url === "/about") { // When another route, the "/about" route, is visited, then...
+        res.setHeader("Content-Type", "text/html")
+        res.write("<h1>About page...</h1>"); // Response with a different title.
+        res.end();
+    }
 });
 
 server.listen("5000", "localhost", () => { // Server listening on specified port and IP address (localhost).
