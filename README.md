286d44a646d48af61c09b3e90a457e880c6ec41a
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2024-04-25 14:05:04 -0600

    4. Create an express app / server practice done, with a few comparisons on node.js standalone routes vs express routes

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # MERN-stack-course
+6. Create an express server and router
+Use the command "npm init", this will create a package.json file, after setting some configurations with the CLI.
+Another way to start a Node.js project, with no previous configurations but only the standards, use the command: "npm init -y".
+
+npmjs.com
+Is the official website of the node package manager, where you can look for librarys or dependencys as expressjs.
+
+Use the command: "npm i express", to install the dependency, add it to the package.json, create the node_modules folder so we can import it and use it for node.js web projects.
---
+++ express-server.js
@@ -0,0 +1,12 @@
+const express = require("express"); // Importing express module to use it.
+
+const app = express(); // Initializing variable app with express() function.
+
+// Routes
+app.get("/", (req, res) => { // We use app.get, to specify the root route, with a callback with a request / response attributes.
+    res.send("<h1>Welcome to the home page...</h2>"); // Sening the response, when the client send a request to the root route.
+})
+
+app.listen("5000", ()=> { // Setting up app to listen on port 5000.
+    console.log("Server running on port 5000"); // Printing out a display message of app listening / running.
+});
